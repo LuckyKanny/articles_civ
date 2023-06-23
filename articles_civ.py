00b2c60689ef49371f301e657651eff1ab4d163c
Oskhane Boya <83473320+LuckyKanny@users.noreply.github.com>
2023-06-23 23:52:57 +0000

    Restructuration du fichier articles_civ.py

--- articles_civ.py
+++ articles_civ.py
@@ -1,40 +1,39 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+"""
+	Description : Lister tous les articles Wikipédia de CIV qui manquent de sources secondaires
+	Auteurs : Samuel Guebo & Paul Bouaffou
+	Contributeur : Oskhane Boya
+	Licence :
+"""
 
-# Description: Lister tous les articles wikipedia  de CIV qui manquent de sources secondaires
-# Auteurs: Samuel Guebo & Paul Bouaffou
-# Licence:
-
-         
 
 import json
 import requests
 
-def logText(filename, text):
+
+def log_text(filename, text):
 	""" Utility for writing in a text file """
-	with open(filename, "a") as f:
+	with open(filename, "a", encoding="utf-8") as f:
 		f.write(text + "\n")
 
-def printNotice(notice):
-    """Print notice message in yellow """
 
-    print('\033[93m' + notice + '\033[0m')
+def print_notice(notice):
+	"""Print notice message in yellow """
+	print('\033[93m' + notice + '\033[0m')
 
-def runMediaWikiRequest(url):
-	""" function to give resultat of url request """
-	return requests.get(url).content
+
+def run_media_wiki_request(url):
+	""" function to give result of url request """
+	result_json = requests.get(url).content
+	# return the json text
+	return result_json
 	
 
 def app():
 	""" Main entry point for the tool. It gets all articles from CIV archives """
-	civ_archives_url = "https://fr.wikipedia.org/w/api.php?action=parse&format=json&page=Projet:C%C3%B4te_d%27Ivoire/Articles_r%C3%A9cents/Archive&prop=links"
-	archives_json = runMediaWikiRequest(civ_archives_url)
+	civ_archives_url = "https://fr.wikipedia.org/w/api.php?action=parse&format=json&page=Projet:C%C3%B4te_d%27Ivoire/Articles_r%C3%A9cents/Archive&prop=links" 
 
-	# convert from plain text to python array, and browse to get items 'parse' and its child 'links'
-	archives_links = json.loads(archives_json)['parse']['links']  
-
-	# just the two first items of the array
-	links_test = archives_links[:500]
+	# convert from plain text to python array, and browse to get items 'parse' and its child 'links' && just the two first items of the array
+	links_test = json.loads(run_media_wiki_request(civ_archives_url))['parse']['links'][0:500]
 
 	# initiate counter
 	articles_count = 0
@@ -42,33 +41,30 @@ def app():
 	for link in links_test:
 		page_title = link['*']
 		# build the url 
-		page_templates_url = "https://fr.wikipedia.org/w/api.php?action=parse&format=json&page="
-		page_templates_url += f"{page_title}&prop=templates"
-
-		# run an Http request and get the template of each page
-		page_templates_json = runMediaWikiRequest(page_templates_url)
-		page_templates = json.loads(page_templates_json)['parse']['templates']
-
+		page_templates_url = f"https://fr.wikipedia.org/w/api.php?action=parse&format=json&page={page_title}&prop=templates"
+		# run a Http request and get the template of each pages
+		page_templates = json.loads(run_media_wiki_request(page_templates_url))['parse']['templates']
 
 		# Make sure the list is not empty
-		if (len(page_templates) > 0):
+		if len(page_templates) > 0:
 			# Define which templates are considered problematic
 			modele_bandeau = [
 				"Modèle:Sources secondaires",
 				"Modèle:Sources",
 				"Modèle:Méta bandeau d'avertissement",
 			]
-
+			
 			for template in page_templates:
 				if template["*"] in modele_bandeau:
 					# save article name into file
-					logText("articles_civ.txt", page_title)
+					log_text("articles_civ.txt", page_title)
 					articles_count += 1
-					print(f"{page_title} has some issues.")
+					print(page_title + " has some issues.")
+
 	# Print total
-	printNotice(f"In total, {str(articles_count)} articles have issues.")
-	if (articles_count > 0):
-		printNotice("Checkout the logs for more details.")
+	print_notice("In total, " + str(articles_count) + " articles have issues.")
+	if articles_count > 0:
+		print_notice("Checkout the logs for more details.")
 
 # Triggering the application
 app()
